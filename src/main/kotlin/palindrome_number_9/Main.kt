2915eb0b6e0ba9d2ec241052be29404770ea2d4d
Evgenii Razboinikov <zhenyarazboynickov@gmail.com>
2022-06-02 10:29:49 +0500

    Added new solution for problem #9

--- src/main/kotlin/palindrome_number_9/Main.kt
+++ src/main/kotlin/palindrome_number_9/Main.kt
@@ -6,8 +6,21 @@ class Solution1 {
         x.toString().reversed() == x.toString()
 }
 
+// Solution from Leetcode
 class Solution {
     fun isPalindrome(x: Int): Boolean {
+        if (x < 0 || x % 10 == 0 && x != 0) {
+            return false
+        }
 
+        var copiedNumber = x
+        var revertedNumber = 0
+
+        while (copiedNumber > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + copiedNumber % 10
+            copiedNumber /= 10
+        }
+
+        return copiedNumber == revertedNumber || copiedNumber == revertedNumber / 10
     }
 }
